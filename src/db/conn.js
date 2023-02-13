8db6605dba74f710fda8f397ed66d0e6a8ac862d
Bhavika Singla <75017244+Bhavikasingla@users.noreply.github.com>
2023-02-13 17:17:42 +0530

    Add files via upload

---
+++ src/db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery" , true);
+
+mongoose.connect('mongodb+srv://Bhavika:bhavii1509ka@cluster0.lkymt6n.mongodb.net/?retryWrites=true&w=majority', { tls: true, ssl: true })
+.then(() => {
+    console.log("Connection Successful!");
+}).catch((err) => {
+    console.log(err);
+}); 
