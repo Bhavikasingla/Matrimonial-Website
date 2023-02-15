f94404737a349cdf27f6d35cb838f9bb13ff972d
Bhavika Singla <75017244+Bhavikasingla@users.noreply.github.com>
2023-02-15 19:56:41 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery" , true);
 
-mongoose.connect('mongodb+srv://Bhavika:bhavii1509ka@cluster0.lkymt6n.mongodb.net/?retryWrites=true&w=majority', { tls: true, ssl: true })
+mongoose.connect('mongodb+srv://Bhavika:<password>@cluster0.lkymt6n.mongodb.net/?retryWrites=true&w=majority', { tls: true, ssl: true })
 .then(() => {
     console.log("Connection Successful!");
 }).catch((err) => {
